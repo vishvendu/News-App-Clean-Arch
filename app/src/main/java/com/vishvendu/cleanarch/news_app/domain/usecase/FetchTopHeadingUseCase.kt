095ac7f410a652efca3660ba20da39871a86e363
psa-vpalawat <vishvendu.palawat@ext.mpsa.com>
2023-01-17 10:49:10 +0530

    Usecase added

---
+++ app/src/main/java/com/vishvendu/cleanarch/news_app/domain/usecase/FetchTopHeadingUseCase.kt
@@ -0,0 +1,17 @@
+package com.vishvendu.cleanarch.news_app.domain.usecase
+
+import com.vishvendu.cleanarch.news_app.data.model.topheadlines.TopHeadlineArticle
+import com.vishvendu.cleanarch.news_app.domain.repository.TopHeadlineRepository
+import com.vishvendu.cleanarch.news_app.utils.AppConstant
+import com.vishvendu.cleanarch.news_app.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FetchTopHeadingUseCase @Inject constructor(private val topHeadlineRepository: TopHeadlineRepository) {
+
+    suspend operator fun invoke() : Resource<List<TopHeadlineArticle>>  =
+        withContext(Dispatchers.IO) {
+            topHeadlineRepository.getTopHeadlinesWithCoroutines(AppConstant.COUNTRY)
+        }
+}
